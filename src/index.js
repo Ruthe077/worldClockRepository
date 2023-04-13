fb6d1f4295c33b8046cdb6ffe6b669e128682b46
Ruthe077 <122920984+Ruthe077@users.noreply.github.com>
2023-04-13 16:37:01 -0500

    updating lviv and houston dates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@
 function houstonTime () {
 let houston = document.querySelector("#houston");
 let houstonTime = moment().tz("America/Chicago").format("h:mm:ss A");
+let houstonDate = document.querySelector("#houston-date");
+let hDate = moment().tz("America/Chicago").format("MMM Do, YYYY");
 
+houstonDate.innerHTML = hDate;
 houston.innerHTML = houstonTime;
 }
 
@@ -11,7 +14,10 @@ setInterval(houstonTime, 1000);
 function lvivTime (){
 let lviv = document.querySelector("#lviv");
 let lvivTime = moment().tz("Europe/Kiev").format("h:mm:ss A");
+let lvivDate = document.querySelector("#lviv-date");
+let lDate = moment().tz("Europe/Kiev").format("MMM Do, YYYY");
 
+lvivDate.innerHTML = lDate;
 lviv.innerHTML = lvivTime;
 }
 setInterval(lvivTime, 1000);
