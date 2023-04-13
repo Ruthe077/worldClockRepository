821b4c77da8fbb0170446d22617b41432d1bec5c
Ruthe077 <122920984+Ruthe077@users.noreply.github.com>
2023-04-13 16:13:22 -0500

    js for static cities

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@
 
 
 
+let houston = document.querySelector("#houston");
+let houstonTime = moment().tz("America/Chicago").format("dddd, MMMM D, YYYY h:m A");
+
+houston.innerHTML = houstonTime;
+
+
+let lviv = document.querySelector("#lviv");
+let lvivTime = moment().tz("Europe/Kiev").format("dddd, MMMM D, YYYY h:m A");
+
+lviv.innerHTML = lvivTime;
+
+
+let sydney = document.querySelector("#sydney");
+let sydneyTime = moment().tz("Australia/Sydney").format("dddd, MMMM D, YYYY h:m A");
+
+sydney.innerHTML = sydneyTime;
+
