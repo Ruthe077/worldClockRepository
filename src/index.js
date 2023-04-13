7c0d27ab060bff8718fcf8eb0f05cec98f1d81bc
Ruthe077 <122920984+Ruthe077@users.noreply.github.com>
2023-04-13 16:21:54 -0500

    updating setintervals in js

--- src/index.js
+++ src/index.js
@@ -6,16 +6,20 @@ let houstonTime = moment().tz("America/Chicago").format("h:mm:ss a");
 houston.innerHTML = houstonTime;
 }
 
-setInterval("houstonTime", 1000)
+setInterval(houstonTime, 1000);
 
+function lvivTime (){
 let lviv = document.querySelector("#lviv");
 let lvivTime = moment().tz("Europe/Kiev").format("h:m A");
 
 lviv.innerHTML = lvivTime;
+}
+setInterval(lvivTime, 1000);
 
-
+function sydneyTime() {
 let sydney = document.querySelector("#sydney");
 let sydneyTime = moment().tz("Australia/Sydney").format("h:m A");
 
 sydney.innerHTML = sydneyTime;
-
+}
+setInterval(sydneyTime, 1000);
